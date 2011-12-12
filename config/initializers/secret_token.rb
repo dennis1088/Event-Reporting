194a0abd3486bfa17d26d1485c2631ae76337936
Dennis Rodriguez <dennis1088@gmail.com>
2011-12-12 14:26:14 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eyor::Application.config.secret_token = 'a6bb2f1c4cdf1058388a75cb5b7692a2a81916acebb6d3864766736e4ce38b56a61cc6818148ba5362e80aa44dd8372105d5ba37a39c367b7bb20271169e9e6f'
